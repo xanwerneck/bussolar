5c44fbf3a03d79d9881e90a06291038cd8d00a39
Alexandre Werneck <xanwerneck@gmail.com>
2021-01-17 19:19:36 -0300

    ajuste no chatbot para retorno na tela de resultados

--- pwa/src/pages/ChatBot.js
+++ pwa/src/pages/ChatBot.js
@@ -8,6 +8,8 @@ import {
 import Chat from "../components/Chat";
 import Menu from "../components/Menu";
 import './type.css'
+import Logo from '../images/logo.png';
+import { getName } from "../utils/auth";
 
 export default class ChatBot extends React.Component {
     constructor(props) {
@@ -19,7 +21,7 @@ export default class ChatBot extends React.Component {
         }
     }
     componentDidMount() {
-        this.addMensagem('O papo vai ser assim, a gente troca uma ideia sobre coisas mais individuais suas primeiro, tudo bem? Exemplo, fala pra mim, o que você ama fazer, sabe, aquilo que você faria ou faz todo dia?', 'left')
+        this.addMensagem('Oi, ' + getName() + ' ;) . O papo vai ser assim, a gente troca uma ideia sobre coisas mais individuais suas primeiro, tudo bem? Exemplo, fala pra mim, o que você ama fazer, sabe, aquilo que você faria ou faz todo dia?', 'left')
     }
 
     addMensagem = (message, side, type = 'text', link = '', nome_arquivo = '') => {
@@ -46,13 +48,48 @@ export default class ChatBot extends React.Component {
         this.setState({concluido : !this.state.concluido, caminho : caminho})
     }
 
+    irResultado = () => {
+        
+        var identificacao_individual = this.state.caminho.filter(item => (item.intent == "identificacao_individual"))
+        var competencias = this.state.caminho.filter(item => (item.intent == "competencias"))
+        var se_ve_trabalhando = this.state.caminho.filter(item => (item.intent == "se_ve_trabalhando"))
+        var visao_de_mundo = this.state.caminho.filter(item => (item.intent == "visao_de_mundo"))
+        var id = []
+        if (identificacao_individual.length > 0) {
+            id = identificacao_individual[0].entities
+        }
+        var co = []
+        if (competencias.length > 0) {
+            co = competencias[0].entities
+        }
+        var se = []
+        if (se_ve_trabalhando.length > 0) {
+            se = se_ve_trabalhando[0].entities
+        }
+        var vi = []
+        if (visao_de_mundo.length > 0) {
+            vi = visao_de_mundo[0].entities
+        }
+        var data = {
+            "identificacao_individual" : id,
+            "competencias" : co,
+            "se_ve_trabalhando" : se,
+            "visao_de_mundo" : vi
+        }
+        this.props.history.push({
+            pathname : '/result',
+            state : {
+                data
+            }
+        })
+    }
+
     render() {
         return (
-            <>
-                <Menu 
-                    goLogin={() => this.props.history.push('/login')}
-                />
-                <Container>
+            <Container style={{position: "absolute", zIndex: 3}}>
+                <div className="wrap-logo" style={{marginBottom: 10}}>
+                    <img src={Logo} alt="Bussolar >" width="100" />
+                </div>
                 <Chat 
                 message_list={this.state.message_list}
                 addMensagem={this.addMensagem}
@@ -62,8 +99,8 @@ export default class ChatBot extends React.Component {
                     this.state.concluido ? (
                         <div style={{margin: 15, marginTop: 30}}>
                             <Button pill block
-                            style={{backgroundColor: "#8C0406", border: 0, fontSize: 20}}
-                            onClick={()=>this.props.history.goBack()}
+                            className="bt-captura"
+                            onClick={()=>this.irResultado()}
                             >
                                 Mostre meus resultados
                                 <FontAwesomeIcon icon={faArrowRight} />
@@ -71,9 +108,7 @@ export default class ChatBot extends React.Component {
                         </div>
                     ) : null
                 }                
-                </Container>
-                
-            </>
+            </Container>
         )
     }
 }
