0d6788cb276dd1f4f74564b51df2149d74147e8b
Vinicius Buzato <viniciusbuzato@gmail.com>
2021-01-17 13:25:11 -0300

    lead-page

--- pwa/src/pages/Captura.js
+++ pwa/src/pages/Captura.js
@@ -1,5 +1,7 @@
 import React from "react";
-import { Container, Row, Button, Form, FormGroup, FormInput } from "shards-react";
+import Logo from '../images/logo.png'
+import { Container, Button, Form, FormGroup, FormInput } from "shards-react";
+import './Captura.css';
 
 export default class Captura extends React.Component {
     constructor(props) {
@@ -17,45 +19,55 @@ export default class Captura extends React.Component {
 
     render() {
         return (
-            <Container >
-                <div style={{textAlign: "center"}}>
-                    <img src="/images/logo-horizontal.jpg" width="96" style={{borderRadius: 10}} />
-                </div>
-                <div style={{marginTop: '20vh'}}>
-                    <h4
-                    style={{color: "#2b0600", marginBottom: 30}}
-                    >
-                        Olá, bem-vindo ao Bussolar, que tal falarmos antes de qualquer coisa sobre você? Assim, a gente já troca os nossos contatos.
-                    </h4>
-                    <Form>
-                        <FormGroup>
-                            <label htmlFor="#email">Nome</label>
-                            <FormInput 
-                            id="#email" placeholder="Nome" 
-                            onChange={(event) => this.setState({nome : event.target.value})}
-                            />
-                        </FormGroup>
-                        <FormGroup>
-                            <label htmlFor="#email">E-mail</label>
-                            <FormInput 
-                            id="#email" placeholder="E-mail" 
-                            onChange={(event) => this.setState({email : event.target.value})}
-                            />
-                        </FormGroup>
-                        <FormGroup>
-                            <label htmlFor="#idade">Idade</label>
-                            <FormInput type="idade" id="#idade" placeholder="idade" 
-                            onChange={(event) => {this.setState({idade : event.target.value})}}
-                            />
-                        </FormGroup>
-                    </Form>
-                    <Button pill block style={{backgroundColor: "#8c0406", border: 0}}
-                    onClick={() => this.acessar()}
-                    >
-                        <strong>Bora começar</strong>
-                    </Button>
-                </div>
-            </Container>
+            <>
+                <div className="left-yellow" />
+                <div className="right-blue" />
+                <Container className="wrap-captura">
+                    
+                    <div className="wrap-logo">
+                        <img src={Logo} alt="Bussolar >" width="250" />
+                    </div>
+                    <div style={{marginTop: '50px'}}>
+                        <h4 className="text-home">
+                            Olá, bem-vindo ao Bussolar, que tal falarmos sobre você antes de qualquer coisa?
+                            Assim, a gente já troca os nossos contatos.
+                        </h4>
+                        <Form>
+                            <FormGroup>
+                                {/* <label htmlFor="#email">Nome</label> */}
+                                <FormInput 
+                                id="#nome" placeholder="Nome" className="nome"
+                                onChange={(event) => this.setState({nome : event.target.value})}
+                                />
+                            </FormGroup>
+                            <FormGroup>
+                                {/* <label htmlFor="#email">E-mail</label> */}
+                                <FormInput 
+                                id="#email" placeholder="E-mail" className="email"
+                                onChange={(event) => this.setState({email : event.target.value})}
+                                />
+                            </FormGroup>
+                            <FormGroup>
+                                {/* <label htmlFor="#idade">Idade</label> */}
+                                <FormInput type="date" id="#idade" placeholder="Data de nascimento" className="idade"
+                                onChange={(event) => {this.setState({idade : event.target.value})}}
+                                />
+                            </FormGroup>
+                        </Form>
+                        <Button pill block className="bt-captura"
+                        onClick={() => this.acessar()}
+                        >
+                            iniciar trilha
+                        </Button>
+                    </div>
+                </Container>
+            </>
         )
     }
 }
+
+{/* <Button pill block className="bt-captura"
+onClick={() => this.acessar()}
+>
+    iniciar trilha
+</Button> */}
